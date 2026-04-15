7efe119fdd39631913df4f4703e559463406e86b
Thinal <r.thinal.fernando@gmail.com>
2026-04-15 12:32:23 +0530

    Created the DiscoveryResource Class

---
+++ src/main/java/com/mycompany/smartcampusapi/resource/DiscoveryResource.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.smartcampusapi.resource;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+
+/**
+ * Author - Thinal Fernando
+ * UOW NO - W2149585
+ * IIT NO - 20242112
+ */
+
+@Path("/")
+public class DiscoveryResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Map<String, Object> getApiInfo() {
+
+        Map<String, Object> response = new HashMap<>();
+
+        response.put("version", "1.0");
+        response.put("admin contact", "w2149585@westminster.ac.uk");
+
+        Map<String, String> resources = new HashMap<>();
+        resources.put("discovery", "/api/v1");
+        resources.put("rooms", "/api/v1/rooms");
+        resources.put("sensors", "/api/v1/sensors");
+        resources.put("sensorReadingsTemplate", "/api/v1/sensors/{sensorId}/readings");
+
+
+        response.put("resources", resources);
+
+        return response;
+    }
+}
