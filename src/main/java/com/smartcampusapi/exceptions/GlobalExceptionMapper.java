344885f63328564988b9f4f3fea34c65ccde38d7
Thinal <r.thinal.fernando@gmail.com>
2026-04-15 14:22:20 +0530

    Created the global safety net

---
+++ src/main/java/com/smartcampusapi/exceptions/GlobalExceptionMapper.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.smartcampusapi.exceptions;
+
+import com.smartcampusapi.model.ErrorMessage;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+/**
+ *
+ * @author User
+ */
+@Provider
+public class GlobalExceptionMapper implements ExceptionMapper<Throwable> {
+    @Override
+    public Response toResponse(Throwable ex) {
+        ErrorMessage error = new ErrorMessage("An unexpected internal server error occurred.", 500, "/api/v1");
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                .type(MediaType.APPLICATION_JSON)
+                .entity(error)
+                .build();
+    }
+}
